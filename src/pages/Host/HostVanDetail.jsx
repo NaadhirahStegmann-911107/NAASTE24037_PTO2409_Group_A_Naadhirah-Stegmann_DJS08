1f2ebcccc4bf7b49d732336f41c7f5ed6f509509
Naadhirah Stegmann <naadhirahsteg@gmail.com>
2025-06-15 16:23:46 +0200

    Host Van details
    Added details for the host van pages as well initial layout for the details of each van.

---
+++ src/pages/Host/HostVanDetail.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from'react';
+
+export default function HostVanDetail() {
+    return (
+        <h1>Host Van Detail Page</h1>
+    );
+}
---
+++ src/pages/Host/HostVans.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+export default function HostVans() {
+    return ( 
+        <h1>Host Vans List Page</h1>
+    )
+}
