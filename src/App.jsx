a78f0a38ffad8601bc21d01c7886011e47cf21fc
Naadhirah Stegmann <naadhirahsteg@gmail.com>
2025-06-15 13:21:45 +0200

    Host Layout
    Creating a better pathway for the routes by making them relative to the main or parent element., and fixed a few syntax typos as well.
    
    Reformating `server.js` file as well.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
-import About from './pages/About';
+import Home from "./pages/Home";
+import About from "./pages/About";
 import Vans from "./pages/Vans/Vans";
-import VanDetail from ".pages/Vans/VanDetail";
+import VanDetail from "./pages/Vans/VanDetail";
 import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
 import Reviews from "./pages/Host/Reviews";
 import Layout from "./components/Layout";
+import HostLayout from "./components/HostLayout";
 
 import "./server"
 
@@ -17,12 +19,15 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />}/>
-          <Route path="/about" element={<About />}/>
-          <Route path="/vans" element={<Vans />}/>
-          <Route path="/vans/:id" element={<VanDetail />}/>
-          <Route path="/host/" element={<Dashboard />}/>
-          <Route path="/host/income" element={<Income />}/>
-          <Route path="/host/reviews" element={<Reviews />}/>
+          <Route path="about" element={<About />}/>
+          <Route path="vans" element={<Vans />}/>
+          <Route path="vans/:id" element={<VanDetail />}/>
+
+          <Route path="/host/" element={<HostLayout />}>
+            <Route path="host" element={<Dashboard />}/>
+            <Route path="income" element={<Income />}/>            
+            <Route path="reviews" element={<Reviews />}/>
+          </Route>  
         </Route>
       </Routes>
     </BrowserRouter>
