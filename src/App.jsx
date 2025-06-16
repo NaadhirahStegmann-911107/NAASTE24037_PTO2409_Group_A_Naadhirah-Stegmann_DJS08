720f95f797cce726757a1382f436cb56ef61d5ac
Naadhirah Stegmann <naadhirahsteg@gmail.com>
2025-06-16 05:48:35 +0200

    Updates
    Discovered typos, amended.

--- src/App.jsx
+++ src/App.jsx
@@ -42,22 +42,24 @@ const router = createBrowserRouter(createRoutesFromElements(
     loader={vansLoader} 
     />
 
-    <Route element={<AuthRequiried />}>
+    <Route element={<AuthRequired />}>
   
-    <Route path="host" element={<HostLayout />}>
-      <Route index element={<Dashboard />}/>
-      <Route path="income" element={<Income />}/>            
-      <Route path="reviews" element={<Reviews />}/>
-      <Route path="vans" element={<HostVans />}/>
+      <Route path="host" element={<HostLayout />}>
+        <Route index element={<Dashboard />}/>
+        <Route path="income" element={<Income />}/>            
+        <Route path="reviews" element={<Reviews />}/>
+        <Route path="vans" element={<HostVans />}/>
 
-      <Route path="vans/:id" element={<HostVanDetail />}>
-      <Route index element={<HostVanInfo />} />
-        <Route path="pricing" element={<HostVanPricing />} />
-        <Route path="photos" element={<HostVanPhotos />} />
-      </Route>  
+        <Route path="vans/:id" element={<HostVanDetail />}>
+          <Route index element={<HostVanInfo />} />
+          <Route path="pricing" element={<HostVanPricing />} />
+          <Route path="photos" element={<HostVanPhotos />} />
+        </Route>  
+      </Route>
+      <Route path="*" element={<NotFound />} />
     </Route>
-    <Route path="*" element={<NotFound />} />
   </Route>
+
 ))
 
 function App() {
